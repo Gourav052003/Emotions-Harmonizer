c63184a459dc3053faa7b2fa5a966db075d0f308
Gourav <81559597+Gourav052003@users.noreply.github.com>
2023-12-09 12:59:40 +0530

    Delete Music_Generation directory

--- Music_Generation/Src/Constants.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-CONFIG_FILE_PATH = Path("Music_Generation/Config.yaml")
-PARAMS_FILE_PATH = Path("Music_Generation/Params.yaml")
-
-REPOSITORY = Path('Gourav052003/Mix-Music-Splitter-Dataset')
-FOLDER = Path('Mix Music')
-SAVE_FOLDER = Path('Artifacts/Dataset')
-
